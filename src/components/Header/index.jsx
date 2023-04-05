bb59b3f2f729a06285a1922f7844197afd306191
Felipe Mendonca <felipemendonca110@gmail.com>
2023-04-04 21:09:36 -0300

    Auth

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,19 @@
 import { MdLogout, MdOutlinePersonOutline } from 'react-icons/md'
 import { Container, Profile, Logout } from './styles'
 
+import {useAuth} from '../../hooks/auth'
+
 export function Header() {
+
+    const { signOut } = useAuth()
+
     return (
         <Container>
             <Profile>
                 <MdOutlinePersonOutline />
                 <p>Gabriel Rennó</p>
             </Profile>
-            <Logout>
+            <Logout onClick={signOut}>
                 <p> Sair </p>
                 <MdLogout />
             </Logout>
--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect } from "react";
 
 import { api } from '../services/api'
 
@@ -6,20 +6,53 @@ export const AuthContext = createContext({})
 
 function AuthProvider({ children }){ 
 
-    // // async function signIn({ username, password }){
-    //     try {
-    //         //     const response = await api.post('/sessions', {email, password})
-    //     } catch (error.response) {
-    //         alert(error.response.data.message)
-    //     } else {
-    //         alert("Nao foi possivel entrar")
-    //     }
+    async function signIn({ username, password }){
+        try {
+              const response = await api.post('/sessions', {username, password})
 
-    // // }
+              const { idUser, token } = response.data;
 
-    return <MyContext.Provider value={{ signIn }}> 
+              localStorage.setItem (`@erp:idUser`, JSON.stringify(idUser))
+              localStorage.setItem (`@erp:token`, token)
+
+              api.defaults.headers.common['Authorization'] = 'Bearer ${token}'
+              setData({ idUser, token })
+        
+            } catch (error.response) {
+            alert(error.response.data.message)
+            } else {
+            alert("Nao foi possivel entrar")
+            }
+
+    }
+
+    function signOut(){
+        const token = localStorage.removeItem("@erp:token")
+        const idUser = localStorage.removeItem("@erp:idUser")
+        
+        setData({});
+    }
+
+    useEffect(() => {
+        const token = localStorage.getItem("@erp:token")
+        const idUser = localStorage.getItem("@erp:idUser")
+        
+        if(token && idUser) {
+            api.defaults.headers.common['Authorization'] = 'Bearer ${token}'
+        
+            setData({
+                token,
+                idUser: JSON.parse(idUser)
+            })
+        }
+    
+    }, [])
+
+    return (
+    <MyContext.Provider value={{ signIn, signOut,  idUser: data.idUser }}> 
         {children}
     </MyContext.Provider>
+    )
 }
 
 function useAuth(){
